a4f05077be8dc066b20109c2f01a09ef504f0870
Szilard Kelemen <szilard_kelemen@outlook.com>
2024-07-19 01:26:00 +0300

    schita

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
+import { useFonts, PressStart2P_400Regular } from '@expo-google-fonts/press-start-2p';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    PressStart2P_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.buttonContainer}>
+      <Pressable style={styles.buttonContainer}>
+        <Text style={styles.textFont1} onPress={()=>Alert.alert("apasat")}>log</Text>
+      </Pressable>
+      </View>
+      <View>
+        <Text style={styles.textFont1}>Sample Ceva</Text>
+        <Text style={styles.welcome}>Welcome!</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +29,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
+    
   },
+  buttonContainer: {
+    alignSelf: 'flex-end',
+    backgroundColor: 'black',
+    paddingTop: '6%',
+  },
+  buttonText: {
+    color: 'black', 
+  },
+  textFont1: {
+    fontFamily: 'PressStart2P_400Regular',
+    fontSize: 30,
+    textAlign: 'center',
+    marginVertical: 20,
+    color: 'yellow',
+  },
+  welcome:{
+    fontFamily: 'PressStart2P_400Regular',
+    fontSize: 30,
+    textAlign: 'center',
+    marginVertical: 20,
+    color: 'yellow',
+    paddingLeft:'7.5%',
+   paddingTop: '95%'
+  }
 });
