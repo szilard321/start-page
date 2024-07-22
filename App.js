3b409b1b1374295f6830b38d39d8df1f27a6e0be
Szilard Kelemen <szilard_kelemen@outlook.com>
2024-07-22 10:54:32 +0300

    new pressable

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
 import { useFonts, PressStart2P_400Regular } from '@expo-google-fonts/press-start-2p';
+import Video from 'react-native-video';
+
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -13,16 +15,19 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+    <View style={styles.header}>
       <View style={styles.buttonContainer}>
-      <Pressable style={styles.buttonContainer}>
-        <Text style={styles.textFont1} onPress={()=>Alert.alert("apasat")}>log</Text>
-      </Pressable>
-      </View>
-      <View>
-        <Text style={styles.textFont1}>Sample Ceva</Text>
-        <Text style={styles.welcome}>Welcome!</Text>
+        <Pressable style={styles.button} onPress={() => Alert.alert("apasat")} >
+          <Text style={styles.textFont2} >log</Text>
+        </Pressable>
       </View>
     </View>
+   
+    <View style={styles.content}>
+      <Text style={styles.textFont1}>Sample</Text>
+      <Text style={styles.welcome}>Welcome!</Text>
+    </View>
+  </View>
   );
 }
 
@@ -30,30 +35,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingTop: '6%',
+    paddingRight: '5%',
   },
   buttonContainer: {
-    alignSelf: 'flex-end',
     backgroundColor: 'black',
-    paddingTop: '6%',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'red',
+    borderRadius: 5,
   },
-  buttonText: {
-    color: 'black', 
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   textFont1: {
     fontFamily: 'PressStart2P_400Regular',
-    fontSize: 30,
+    fontSize: 20,
     textAlign: 'center',
-    marginVertical: 20,
     color: 'yellow',
   },
-  welcome:{
+  textFont2: {
+    fontFamily: 'PressStart2P_400Regular',
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'yellow',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
     fontFamily: 'PressStart2P_400Regular',
     fontSize: 30,
     textAlign: 'center',
     marginVertical: 20,
     color: 'yellow',
-    paddingLeft:'7.5%',
-   paddingTop: '95%'
+    paddingLeft: '7.5%',
+    paddingTop: '95%',
+  },
+  backgroundVideo: {
+    width: '100%',
+    height: 300,
   }
 });
